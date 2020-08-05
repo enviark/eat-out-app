40b9708b7937b2f74b6c37a364252b8abaa5d0c3
Kailan Blanks <kailan@enviark.com>
2020-08-06 00:14:16 +0100

    Fetch and store postcode on map region update

--- components/RestaurantMapView.tsx
+++ components/RestaurantMapView.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, { Region } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function RestaurantMapView() {
+  const [postCode, setPostCode] = React.useState<string | null>()
+  const [restaurants, setRestaurants] = React.useState([])
+
+  const onChange = async (region: Region) => {
+    // use `setRestaurants` to update markers based on restaurants in `region`
+    let locs = await Location.reverseGeocodeAsync(region);
+    if (locs.length > 0) {
+      let loc = locs[0];
+      if (loc.postalCode && loc.postalCode.length > 4) {
+        setPostCode(loc.postalCode);
+      }
+    }
+  }
+  
   return (
     <View style={styles.container}>
+      <Text>
+        {postCode}
+      </Text>
       <MapView
         style={styles.map}
         showsUserLocation={true}
         showsMyLocationButton={true}
         showsPointsOfInterest={false}
-      />
+        onRegionChangeComplete={onChange}
+      >
+        {
+          // render <Marker> for each item in `restaurants`
+        }
+      </MapView>
     </View>
   );
 }
