7685ff359a160b8c904c1a3d244b4a5546673857
Kailan Blanks <kailan@enviark.com>
2020-08-06 02:53:19 +0100

    Add AdMob ads to bottom of map screen

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import { osName } from 'expo-device';
+
 import RestaurantMapView from '../components/RestaurantMapView';
+import { AdMobBanner } from 'expo-ads-admob';
+
+const adUnit = (osName == 'iOS' || osName == 'iPadOS') ? 'ca-app-pub-2883417370695291/7147386410' : 'ca-app-pub-2883417370695291/1359873114';
 
 export default function MapScreen() {
   return (
     <View style={styles.container}>
       <RestaurantMapView />
+      <AdMobBanner
+        bannerSize="banner"
+        adUnitID={adUnit}
+      />
     </View>
   );
 }
