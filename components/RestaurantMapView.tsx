c605d1afdb59bafecfaba2290733490ac5443123
Kailan Blanks <kailan@enviark.com>
2020-08-06 01:46:42 +0100

    Load restaurants from API

--- components/RestaurantMapView.tsx
+++ components/RestaurantMapView.tsx
@@ -1,29 +1,52 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-import MapView, { Region } from 'react-native-maps';
-import * as Location from 'expo-location';
+import MapView, { Region, Marker, LatLng } from 'react-native-maps';
+
+interface Restaurant {
+  id: string,
+  location: LatLng,
+  name: string
+}
+
+function mapResponseToRestaurant(resp: any): Restaurant {
+  return {
+    id: resp._id,
+    name: resp.name,
+    location: {
+      latitude: resp.location.coordinates[1],
+      longitude: resp.location.coordinates[0]
+    }
+  }
+}
 
 export default function RestaurantMapView() {
-  const [postCode, setPostCode] = React.useState<string | null>()
-  const [restaurants, setRestaurants] = React.useState([])
+  const [restaurants, setRestaurants] = React.useState<Restaurant[]>([])
 
   const onChange = async (region: Region) => {
     // use `setRestaurants` to update markers based on restaurants in `region`
-    let locs = await Location.reverseGeocodeAsync(region);
-    if (locs.length > 0) {
-      let loc = locs[0];
-      if (loc.postalCode && loc.postalCode.length > 4) {
-        setPostCode(loc.postalCode);
-      }
+
+    let url = new URL('https://api.eatoutmap.uk/api/establishments');
+
+    let resp = await fetch('https://api.eatoutmap.uk/api/establishments', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(region)
+    });
+    
+    let data = await resp.json();
+    let locations = data.message;
+
+    if (locations.length > 0) {
+      setRestaurants(locations.map(mapResponseToRestaurant));
     }
   }
   
   return (
     <View style={styles.container}>
-      <Text>
-        {postCode}
-      </Text>
       <MapView
         style={styles.map}
         showsUserLocation={true}
@@ -31,9 +54,13 @@ export default function RestaurantMapView() {
         showsPointsOfInterest={false}
         onRegionChangeComplete={onChange}
       >
-        {
-          // render <Marker> for each item in `restaurants`
-        }
+        {restaurants.map((restaurant) => {
+          return <Marker
+            key={restaurant.id}
+            coordinate={restaurant.location}
+            title={restaurant.name}
+          />
+        })}
       </MapView>
     </View>
   );
